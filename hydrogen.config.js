93bd7755e37c0c4af3785d275bb9cf014b5104f7
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-05-31 14:19:59 +0200

    added nosto storefront token and domain for testing

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -7,8 +7,10 @@ import {
 export default defineConfig({
   routes: import.meta.globEager('./src/routes/**/*.server.[jt](s|sx)'),
   shopify: {
-    storeDomain: 'hydrogen-preview.myshopify.com',
-    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+    //storeDomain: 'hydrogen-preview.myshopify.com',
+    //storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+    storeDomain: 'nosto-hydrogen.myshopify.com',
+    storefrontToken: '3ad9cd17e9b93845eba336765e0f33ac',
     storefrontApiVersion: '2022-07',
   },
   session: CookieSessionStorage('__session', {
