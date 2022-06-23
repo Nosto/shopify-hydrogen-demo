87b0c103cf309dfbd4ccb1b797eb5c14ce1d4d16
Dominik Gilg <dominikgilg@Dominiks-MBP.fritz.box>
2022-06-23 15:14:38 +0200

    Added initial code

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,18 @@
+/// <reference types="vitest" />
 import {defineConfig} from 'vite';
 import hydrogen from '@shopify/hydrogen/plugin';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [hydrogen()],
-  optimizeDeps: {include: ['@headlessui/react']},
+  resolve: {
+    alias: [{find: /^~\/(.*)/, replacement: '/src/$1'}],
+  },
+  optimizeDeps: {
+    include: ['@headlessui/react', 'clsx', 'react-use', 'typographic-base'],
+  },
   test: {
     globals: true,
     testTimeout: 10000,
     hookTimeout: 10000,
   },
-  ssr: {noExternal: ['@nosto/nosto-react']},
 });
