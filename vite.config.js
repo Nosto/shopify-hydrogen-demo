bb5afbd2cfc9b10d087e28a3cda37701f8694243
Dominik Gilg <dominikgilg@Dominiks-MBP.fritz.box>
2022-06-24 19:06:38 +0200

    changed vite.config and @nosto dependency

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,13 @@ export default defineConfig({
     alias: [{find: /^~\/(.*)/, replacement: '/src/$1'}],
   },
   optimizeDeps: {
-    include: ['@headlessui/react', 'clsx', 'react-use', 'typographic-base'],
-    exclude: ['@nosto/nosto-react'],    
+    include: [
+      '@headlessui/react',
+      'clsx',
+      'react-use',
+      'typographic-base',
+      '@nosto/nosto-react',
+    ],
   },
   test: {
     globals: true,
