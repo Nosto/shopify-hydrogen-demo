aa5105caf6f3c2716f6356b765fe0226e19516b4
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-06-25 11:15:45 +0200

    Set defaultCountryCode to FI

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -2,7 +2,7 @@ import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    defaultCountryCode: 'US',
+    defaultCountryCode: 'FI',
     defaultLanguageCode: 'EN',
     storeDomain: 'hydrogen-preview.myshopify.com',
     storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
