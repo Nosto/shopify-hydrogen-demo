2e22829379d625faf9c1833ecdaa69be5dc7687f
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-08-02 11:42:36 +0300

    edited build script

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -2,15 +2,10 @@ import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: () => ({
-    defaultCountryCode: 'US',
+    defaultCountryCode: 'FI',
     defaultLanguageCode: 'EN',
-    storeDomain:
-      // @ts-ignore
-      Oxygen?.env?.SHOPIFY_STORE_DOMAIN || 'hydrogen-preview.myshopify.com',
-    storefrontToken:
-      // @ts-ignore
-      Oxygen?.env?.SHOPIFY_STOREFRONT_API_PUBLIC_TOKEN ||
-      '3b580e70970c4528da70c98e097c2fa0',
+    storeDomain: 'hydrogen-preview.myshopify.com',
+    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
     storefrontApiVersion: '2022-07',
   }),
   nosto: {
