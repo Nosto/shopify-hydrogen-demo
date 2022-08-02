b05eb57d32983855208413a6daeb5bcb06d42b2f
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-08-02 17:55:05 +0300

    added logic for extracting requried data from the Shopify cart object and pass it to the NostoSession component

--- src/components/NostoComponents.client.jsx
+++ src/components/NostoComponents.client.jsx
@@ -1,9 +1,36 @@
 import * as NostoComponents from '@nosto/nosto-react';
-import {useLocalization, fetchSync} from '@shopify/hydrogen';
+import {useLocalization, fetchSync, useCart, useShop} from '@shopify/hydrogen';
 
 export default function NostoComponent({type, ...props}) {
-  const typeSpecificProps = {};
+  const {storeDomain} = useShop();
   const {country} = useLocalization();
+  const typeSpecificProps = {};
+
+  if (type == 'NostoSession') {
+    const shopifyCart = useCart();
+
+    const nostoCart = [];
+    shopifyCart.lines.forEach((item) => {
+      let handle = item?.merchandise?.product?.handle;
+      let product_id =
+        fetchSync(`https://${storeDomain}/products/${handle}.js`).json()?.id ||
+        undefined;
+      let sku_id = item?.merchandise?.id.split('/').at(-1) || undefined;
+      let quantity = item?.quantity;
+      let unit_price = item?.merchandise?.priceV2?.amount;
+      let price_currency_code = item?.merchandise?.priceV2?.currencyCode;
+
+      nostoCart.push({
+        product_id,
+        sku_id,
+        quantity,
+        unit_price,
+        price_currency_code,
+      });
+
+      typeSpecificProps.cart = {items: nostoCart};
+    });
+  }
 
   //Get currentVariation based on country:
   if (type == 'NostoProvider' && props.multiCurrency) {
