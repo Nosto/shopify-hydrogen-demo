f7ce1c293cf7daeb768e7e28766b4da808445cb9
dominikgilg <74245822+dominikgilg@users.noreply.github.com>
2022-08-03 10:54:05 +0200

    CT-23  Sync cart with nosto (#21)
    * added NostoSession as Child of Nostoprovider
    
    * added logic for extracting requried data from the Shopify cart object and pass it to the NostoSession component
    
    * updated nosto-react dependency
    
    * updated package version

--- src/components/NostoComponents.client.jsx
+++ src/components/NostoComponents.client.jsx
@@ -1,9 +1,53 @@
 import * as NostoComponents from '@nosto/nosto-react';
-import {useLocalization, fetchSync} from '@shopify/hydrogen';
+import {useLocalization, fetchSync, useCart, useShop} from '@shopify/hydrogen';
+import React from 'react';
+import useDeepCompareEffect from 'use-deep-compare-effect';
 
 export default function NostoComponent({type, ...props}) {
-  const typeSpecificProps = {};
+  const {storeDomain} = useShop();
   const {country} = useLocalization();
+  const typeSpecificProps = {};
+  const shopifyCart = useCart();
+
+  const [nostoCart, setNostoCart] = React.useState([]);
+  useDeepCompareEffect(() => {
+    if (type == 'NostoSession') {
+      const nostoCartCopy = [];
+      let promises = [];
+      shopifyCart.lines.forEach((item) => {
+        let promise = new Promise((resolve) => {
+          let handle = item?.merchandise?.product?.handle;
+          let sku_id = item?.merchandise?.id.split('/').at(-1) || undefined;
+          let quantity = item?.quantity;
+          let unit_price = item?.merchandise?.priceV2?.amount;
+          let price_currency_code = item?.merchandise?.priceV2?.currencyCode;
+
+          fetch(`https://${storeDomain}/products/${handle}.js`)
+            .catch(resolve)
+            .then((resp) => resp.json())
+            .then((resp) => {
+              let product_id = resp.id || undefined;
+              nostoCartCopy.push({
+                product_id,
+                sku_id,
+                quantity,
+                unit_price,
+                price_currency_code,
+              });
+              resolve();
+            });
+        });
+        promises.push(promise);
+      });
+      Promise.allSettled(promises).then(() => {
+        setNostoCart(nostoCartCopy);
+      });
+    }
+  }, [shopifyCart.lines]);
+
+  if (type == 'NostoSession') {
+    typeSpecificProps.cart = {items: nostoCart};
+  }
 
   //Get currentVariation based on country:
   if (type == 'NostoProvider' && props.multiCurrency) {
