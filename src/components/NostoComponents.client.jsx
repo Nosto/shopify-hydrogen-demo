ee713538a1dfd93619212c4e0b48b05076994a89
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-08-03 11:43:20 +0300

    added support for syncronising Shopify cart to Nosto

--- src/components/NostoComponents.client.jsx
+++ src/components/NostoComponents.client.jsx
@@ -1,35 +1,52 @@
 import * as NostoComponents from '@nosto/nosto-react';
 import {useLocalization, fetchSync, useCart, useShop} from '@shopify/hydrogen';
+import React from 'react';
+import useDeepCompareEffect from 'use-deep-compare-effect';
 
 export default function NostoComponent({type, ...props}) {
   const {storeDomain} = useShop();
   const {country} = useLocalization();
   const typeSpecificProps = {};
+  const shopifyCart = useCart();
+
+  const [nostoCart, setNostoCart] = React.useState([]);
+  useDeepCompareEffect(() => {
+    if (type == 'NostoSession') {
+      const nostoCartCopy = [];
+      let promises = [];
+      shopifyCart.lines.forEach((item) => {
+        let promise = new Promise((resolve) => {
+          let handle = item?.merchandise?.product?.handle;
+          let sku_id = item?.merchandise?.id.split('/').at(-1) || undefined;
+          let quantity = item?.quantity;
+          let unit_price = item?.merchandise?.priceV2?.amount;
+          let price_currency_code = item?.merchandise?.priceV2?.currencyCode;
+
+          fetch(`https://${storeDomain}/products/${handle}.js`)
+            .catch(resolve)
+            .then((resp) => resp.json())
+            .then((resp) => {
+              let product_id = resp.id || undefined;
+              nostoCartCopy.push({
+                product_id,
+                sku_id,
+                quantity,
+                unit_price,
+                price_currency_code,
+              });
+              resolve();
+            });
+        });
+        promises.push(promise);
+      });
+      Promise.allSettled(promises).then(() => {
+        setNostoCart(nostoCartCopy);
+      });
+    }
+  }, [shopifyCart.lines]);
 
   if (type == 'NostoSession') {
-    const shopifyCart = useCart();
-
-    const nostoCart = [];
-    shopifyCart.lines.forEach((item) => {
-      let handle = item?.merchandise?.product?.handle;
-      let product_id =
-        fetchSync(`https://${storeDomain}/products/${handle}.js`).json()?.id ||
-        undefined;
-      let sku_id = item?.merchandise?.id.split('/').at(-1) || undefined;
-      let quantity = item?.quantity;
-      let unit_price = item?.merchandise?.priceV2?.amount;
-      let price_currency_code = item?.merchandise?.priceV2?.currencyCode;
-
-      nostoCart.push({
-        product_id,
-        sku_id,
-        quantity,
-        unit_price,
-        price_currency_code,
-      });
-
-      typeSpecificProps.cart = {items: nostoCart};
-    });
+    typeSpecificProps.cart = {items: nostoCart};
   }
 
   //Get currentVariation based on country:
