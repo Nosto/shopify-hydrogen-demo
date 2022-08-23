34ae2c48b2e622b54cf8eb9e4a8139708453cca3
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-08-23 09:32:30 +0200

    updated NostoComponent to extract product_id from the Shopify cart directly (possible since latest hydrogen updates)

--- src/components/NostoComponents.client.jsx
+++ src/components/NostoComponents.client.jsx
@@ -1,62 +1,35 @@
 import * as NostoComponents from '@nosto/nosto-react';
-import {useLocalization, fetchSync, useCart, useShop} from '@shopify/hydrogen';
+import {useLocalization, fetchSync, useCart} from '@shopify/hydrogen';
 import React from 'react';
 import useDeepCompareEffect from 'use-deep-compare-effect';
 
 export default function NostoComponent({type, ...props}) {
-  const {storeDomain, storefrontToken} = useShop();
   const {country} = useLocalization();
   const typeSpecificProps = {};
   const shopifyCart = useCart();
-
   const [nostoCart, setNostoCart] = React.useState([]);
+
+  //Sync Shopify cart with Nosto:
   useDeepCompareEffect(() => {
     if (type == 'NostoSession') {
       const nostoCartCopy = [];
-      let promises = [];
       shopifyCart.lines.forEach((item) => {
-        let promise = new Promise((resolve) => {
-          let handle = item?.merchandise?.product?.handle;
-          let sku_id = item?.merchandise?.id.split('/').at(-1) || undefined;
-          let quantity = item?.quantity;
-          let unit_price = item?.merchandise?.priceV2?.amount;
-          let price_currency_code = item?.merchandise?.priceV2?.currencyCode;
+        let product_id =
+          item?.merchandise?.product.id.split('/').at(-1) || undefined;
+        let sku_id = item?.merchandise?.id.split('/').at(-1) || undefined;
+        let quantity = item?.quantity;
+        let unit_price = item?.merchandise?.priceV2?.amount;
+        let price_currency_code = item?.merchandise?.priceV2?.currencyCode;
 
-          let query = `query {
-            productByHandle(handle: "${handle}") {
-              id
-            }
-          }`;
-
-          fetch(`https://${storeDomain}/api/2022-07/graphql.json`, {
-            method: 'post',
-            headers: {
-              'Content-Type': 'application/graphql',
-              'X-Shopify-Storefront-Access-Token': storefrontToken,
-            },
-            body: query,
-          })
-            .catch(resolve)
-            .then((resp) => resp.json())
-            .then((resp) => {
-              if (resp?.data?.productByHandle?.id) {
-                let product_id = resp.data.productByHandle.id.split('/').at(-1);
-                nostoCartCopy.push({
-                  product_id,
-                  sku_id,
-                  quantity,
-                  unit_price,
-                  price_currency_code,
-                });
-              }
-              resolve();
-            });
+        nostoCartCopy.push({
+          product_id,
+          sku_id,
+          quantity,
+          unit_price,
+          price_currency_code,
         });
-        promises.push(promise);
       });
-      Promise.allSettled(promises).then(() => {
-        setNostoCart(nostoCartCopy);
-      });
+      setNostoCart(nostoCartCopy);
     }
   }, [shopifyCart.lines]);
 
