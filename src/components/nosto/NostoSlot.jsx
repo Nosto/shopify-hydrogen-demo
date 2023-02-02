e1114814e4d0fa2ade5f4d37939bd707f30e4aae
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2023-02-02 13:08:25 +0100

    implemented carousel

--- src/components/nosto/NostoSlot.jsx
+++ src/components/nosto/NostoSlot.jsx
@@ -1,9 +1,31 @@
-export function NostoItem({recommendation}) {
+import {NostoItem} from './NostoItem';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+export function NostoSlot({recommendation}) {
   let {title, result_id, products} = recommendation;
+
+  let carouselSettings = {
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    infinite: true,
+    responsive: [
+      {breakpoint: 1024, settings: {slidesToShow: 3}},
+      {breakpoint: 768, settings: {slidesToShow: 2}},
+    ],
+  };
 
   return (
     <div className="nosto-container">
-      <div className="nosto-title"></div>
+      <div className="nosto-title">{title}</div>
+      <div className="nosto-carousel">
+        <Slider {...carouselSettings}>
+          {products.map((product) => (
+            <NostoItem key={product.productId} product={product} />
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 }
