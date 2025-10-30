fe29c4e51a853ab58783ffa85479bbd2be93015a
Timo Westkämper <timo.westkamper@nosto.com>
2025-10-30 15:15:48 +0200

    chore: fix CJS/ESM issues

--- vite.config.js
+++ vite.config.js
@@ -30,7 +30,7 @@ export default defineConfig({
        * Include 'example-dep' in the array below.
        * @see https://vitejs.dev/config/dep-optimization-options
        */
-      include: ['set-cookie-parser', 'cookie', 'react-router'],
+      include: ['set-cookie-parser', 'cookie', 'react-router', 'react-dom', 'react-dom/client'],
     },
   },
   server: {
